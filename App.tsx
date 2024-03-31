e2adfa19b3b1fdc82df42a3cf3e32d0a7759b685
Noe Osorio <osorionoe@gmail.com>
2024-03-30 20:19:11 -0600

    Init app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Image, Text, TouchableOpacity, Linking, SafeAreaView } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>100¥</Text>
+      </View>
+      <Image source={require('./assets/coinFront.png')} style={styles.image} />
+      <Image source={require('./assets/logo.png')} style={styles.logo} />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121212',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+  },
+  header: {
+    backgroundColor: '#D6F35C',
+    width: '100%',
+    padding: 20,
+  },
+  headerText: {
+    textAlign: 'center',
+    color: '#3A566E',
+    fontSize: 24,
+  },
+  image: {
+    flex:10,
+    width: 300,
+    height: 300,
+  },
+  footer: {
+    flex:1,
+    width:"100%",
+    flexDirection: 'row',
+    backgroundColor: '#4BA0A3',
+    padding: 20,
+    display:"flex",
+    justifyContent:"center"
+  },
+  footerText: {
+    borderRadius: 16
+  },
+  linkText: {
+    color: '#A1C4D4',
+  },
+  logo: {
+    flex:2,
+    width: 500,
+    height: 100,
+    objectFit:"contain"
   },
 });
+
+export default App;
